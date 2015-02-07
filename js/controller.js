31e6a89d8bcc3cd76b48b73347e1387fd83732bc
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-02-07 15:36:18 -0700

    add 'addFriend' function to controller

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,10 @@
-var app = angular.module("MainController");
+var app = angular.module("friendsList");
+
+app.controller('MainController', function($scope) {
+    $scope.friendArr = ['Bobby', 'Rachel', 'Peter', 'Ben', 'Susie'];
+    
+     $scope.addFriend = function() {
+        $scope.friendArr.push($scope.newFriend);
+        $scope.newFriend = '';
+     }
+});
